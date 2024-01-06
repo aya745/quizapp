7916f5fdca3206c7de53cda7debeaaef964b240b
aya chouate <chouateaya@gmail.com>
2024-01-06 20:40:06 +0100

    create myapp widget

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 const Color correct = Color(0xFF32AB58);
 //incorrect answer's color
 const Color incorrect = Color(0xFFAB3232);
-//neutral color
+//neutral color for neutral answer
 const Color neutral = Color(0xFFE7E7E7);
-//background color
+//background color 
 const Color backround=Color(0xFF1A3464);
 
