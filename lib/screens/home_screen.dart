7a867f030843f08023fee8435a817d3d2b8fc8b2
aya chouate <chouateaya@gmail.com>
2024-01-06 21:19:28 +0100

    added question widget to homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/constants.dart';
 import 'package:quiz_app/models/question_model.dart';
-
+import 'package:quiz_app/widgets/question_widget.dart';
 //create homescreen widget
 
 class HomeScreen extends StatefulWidget {
@@ -17,19 +17,19 @@ class _HomeScreenState extends State<HomeScreen> {
     Question(
      id: '10',
      title: 'What is 2 + 2 ?', 
-     options: {'5': false, '30': false, '4': true, '10';false},
+     options: {'5': false, '30': false, '4': true, '10': false},
     ),
     Question(
      id: '11',
      title: 'What is 10 + 20 ?', 
-     options: {'50': false, '30': true, '40': false, '10';false},
+     options: {'50': false, '30': true, '40': false, '10': false},
     ),
 
   ];
 
   //create an index to loop through questions
   int index = 0;
-  
+
   @override
   Widget build(BuildContext context) {
   return Scaffold(
@@ -39,6 +39,20 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: backround,
         shadowColor: Colors.transparent,
         ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              //adding questionWidget here
+              QuestionWidget(
+                indexAction: index, //currently at 0
+                question: _questions[index].title, //first in the list
+                totalQuestions: _questions.length, //total length of the list
+                )
+
+            ],
+          ),
+         ),
     );
   }
 }
